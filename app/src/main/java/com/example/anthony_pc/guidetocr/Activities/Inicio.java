d8085900a31567ef678c962b053368830434a0fd
Maria <marilau63@gmail.com>
2018-06-10 20:41:14 -0600

    no message

--- app/src/main/java/com/example/anthony_pc/guidetocr/Activities/Inicio.java
+++ app/src/main/java/com/example/anthony_pc/guidetocr/Activities/Inicio.java
@@ -25,6 +25,7 @@ public class Inicio extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,8 +41,12 @@ public class Inicio extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Guide To CR");
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.getMenu().getItem(0).setChecked(true);
     }
 
     @Override
@@ -81,20 +86,16 @@ public class Inicio extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-/*
-        if (id == R.id.nav_camera) {
+
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_place) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_diccionario) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_exit) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }*/
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
