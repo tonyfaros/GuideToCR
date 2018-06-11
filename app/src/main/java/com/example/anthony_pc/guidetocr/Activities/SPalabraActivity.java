de1cf7d53b72d1bf37c183857a8912f9308f16f5
Maria <marilau63@gmail.com>
2018-06-11 05:04:49 -0600

    detalles lugar sugerir lugar

---
+++ app/src/main/java/com/example/anthony_pc/guidetocr/Activities/SPalabraActivity.java
@@ -0,0 +1,14 @@
+package com.example.anthony_pc.guidetocr.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SPalabraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalabra);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
--- app/src/main/java/com/example/anthony_pc/guidetocr/Fragments/Lugares.java
+++ app/src/main/java/com/example/anthony_pc/guidetocr/Fragments/Lugares.java
@@ -7,6 +7,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -64,6 +65,8 @@ public class Lugares extends Fragment {
         return view;
 
     }
+
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
