adcd584c59eb5f072539e01d12085a8c5fc97f2f
Maria <marilau63@gmail.com>
2018-06-12 13:16:17 -0600

    *palabra activity

--- app/src/main/java/com/example/anthony_pc/guidetocr/Activities/Palabra_activity.java
+++ app/src/main/java/com/example/anthony_pc/guidetocr/Activities/Palabra_activity.java
@@ -11,5 +11,6 @@ public class Palabra_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palabra_activity);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
